50a0b74d90a4c0337dc4f11b3e8b000c8ac50a13
Nishant Kaushish <kaushish.nishant@abc.net.au>
2020-09-03 10:33:44 +1000

    created simple component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Simple from "./components/01.Simple";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to RTL Live Coding</h1>
+      <Simple text="I am simple" />
     </div>
   );
 }
